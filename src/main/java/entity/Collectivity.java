251f23deb72b0fd79be2682d428c3ac94259c743
Yohan-Ralambo <yohanralambo@gmail.com>
2026-04-21 16:28:25 +0300

    entity of collectivity

---
+++ src/main/java/entity/Collectivity.java
@@ -0,0 +1,17 @@
+package entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Collectivity {
+    private Integer id;
+    private String number;
+    private String name;
+    private String specialty;
+    private String city;
+    private Double annualFee;
+}
