658ae33275cffbd9718365d25b195ed9b40ee48c
yox101 <yohanralambo@gmail.com>
2026-05-07 10:49:30 +0300

    feat: secure-key

---
+++ src/main/java/mg/yoan/finaltd/security/ApiKeyFilter.java
@@ -0,0 +1,35 @@
+package mg.yoan.finaltd.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class ApiKeyFilter extends OncePerRequestFilter {
+
+    @Value("${app.api.key}")
+    private String apiKey;
+
+    private static final String API_KEY_HEADER = "x-api-key";
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String requestApiKey = request.getHeader(API_KEY_HEADER);
+
+        if (apiKey.equals(requestApiKey)) {
+            filterChain.doFilter(request, response);
+        } else {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.setContentType("text/plain");
+            response.getWriter().write("Bad credentials");
+        }
+    }
+}
